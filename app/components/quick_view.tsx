eb9ffcd574116c46d6ce00a8273eb24e938ef7a3
AymenBraikia <aymenbraikia1@gmail.com>
2026-06-08 21:41:12 +0100

    clean up

--- app/components/quick_view.tsx
+++ app/components/quick_view.tsx
@@ -13,7 +13,7 @@ export default function QuickViewModal({ view, onClose, format }: QuickViewProps
 	const { addToCart } = useCart();
 
 	return (
-		<div tabIndex={0} role="banner" className="fixed left-0 top-0 w-dvw h-dvh bg-black/50 fade-in flex-center z-10" onClick={(e) => e.target === e.currentTarget && onClose()}>
+		<div tabIndex={0} role="dialog" className="fixed left-0 top-0 w-dvw h-dvh bg-black/50 fade-in flex-center z-10" onClick={(e) => e.target === e.currentTarget && onClose()}>
 			<div className="w-full h-[calc(100%-100px)] translate-y-12.5 p-4 lg:translate-y-0 lg:w-3/5 lg:h-150 lg:min-h-100 lg:p-16 flex-center flex-col lg:flex-row gap-16 font-secondary bg-background relative">
 				<div className="relative w-full h-1/2 lg:w-1/2 lg:h-full">
 					<Image src={view.src} sizes="(maxWidth: 100dvw) 100vw, 100dvw" fill alt={view.name} className="object-cover select-none" />
