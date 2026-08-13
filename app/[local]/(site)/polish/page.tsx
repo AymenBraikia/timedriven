61f4eb5e2cb36048689d67461b9bcdcfef697de1
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-08-13 17:58:16 +0100

    translated polish page

--- app/[local]/(site)/polish/page.tsx
+++ app/[local]/(site)/polish/page.tsx
@@ -1,48 +1,61 @@
-export default function polishPage() {
+import { getTranslations } from "next-intl/server";
+
+export default async function polishPage() {
+    const t = await getTranslations("polish");
+    const polishItems = t.raw("polishing.items") as {
+        title: string;
+        text: string;
+    }[];
+
+    const maintenanceItems = t.raw("maintenance.items") as {
+        title: string;
+        text: string;
+    }[];
+
     return (
         <div className="flex flex-col justify-start items-start gap-6 py-6 sm:px-20 px-6 mt-10">
-            <h1 className="font-semibold font-secondary">Polishing and Service</h1>
-            <p className="tracking-widest leading-6">
-                At Timedriven, we offer a first-class polishing and repair service for top-class brands such as Rolex, IWC, Tudor, Omega and Cartier. Our experienced watchmaker will get your watch in top shape – from precise polishing to
-                professional maintenance.
-            </p>
-            <h2 className="font-secondary">Our services at a glance:</h2>
+            <h1 className="font-semibold font-secondary">{t("heading")}</h1>
+            <p className="tracking-widest leading-6">{t("intro")}</p>
+            <h2 className="font-secondary">{t("servicesHeading")}</h2>
 
             <div className="flex flex-col justify-start items-start gap-2">
-                <h5 className="font-medium font-secondary">Polishing & reconditioning:</h5>
-                <p className="tracking-widest leading-6">We remove scratches and minor signs of wear, restoring your watch to its original shine:</p>
+                <h5 className="font-medium font-secondary">{t("polishing.heading")}</h5>
+                <p className="tracking-widest leading-6">{t("polishing.intro")}</p>
                 <p className="tracking-widest leading-6 text-sm">
-                    - Polishing & Refurbishment from €175 <br />- Polishing including material application & refurbishment from €249
+                    {polishItems.map((e) => (
+                        <>
+                            - {e} <br />
+                        </>
+                    ))}
                 </p>
             </div>
 
             <div className="flex flex-col justify-start items-start gap-2">
-                <h5 className="font-medium font-secondary">Maintenance & repair:</h5>
-                <p className="tracking-widest leading-6">We carry out all necessary repairs and provide a reliable maintenance service for your movement in our master workshop:</p>
+                <h5 className="font-medium font-secondary">{t("maintenance.heading")}</h5>
+                <p className="tracking-widest leading-6">{t("maintenance.intro")}</p>
                 <p className="tracking-widest leading-6 text-sm">
-                    - Battery replacement for standard quartz models €45 <br />
-                    - Rate adjustment / reulation of the time deviation per day €79 <br />
-                    - Dial replacement (3 hands) €199 <br />
-                    - Dial replacement (chronograph) €249 <br />
-                    - Service for 3-hand manual wind watches from €499 <br />
-                    - Service for 3-hand automatic watches from €599 (e.g. Rolex, OMEGA, Panerai, etc.) <br />- Service for chronographs from €749 (e.g. Rolex, OMEGA)
+                    {maintenanceItems.map((e) => (
+                        <>
+                            - {e} <br />
+                        </>
+                    ))}
                 </p>
-                <p className="tracking-widest leading-6">Plus spare parts if necessary. For individual repairs, a cost estimate will be provided.</p>
+                <p className="tracking-widest leading-6">{t("maintenance.note")}</p>
             </div>
 
             <div className="flex flex-col justify-start items-start gap-2">
-                <h5 className="font-medium font-secondary">Expert professionals:</h5>
-                <p className="tracking-widest leading-6">Our watchmaker specializes in the care and repair of luxury watches.</p>
+                <h5 className="font-medium font-secondary">{t("expertHeading")}</h5>
+                <p className="tracking-widest leading-6">{t("expertText")}</p>
             </div>
 
             <div className="flex flex-col justify-start items-start gap-2">
-                <h5 className="font-medium font-secondary">Highest quality:</h5>
-                <p className="tracking-widest leading-6">We only use specialized tools that meet the highest standards.</p>
+                <h5 className="font-medium font-secondary">{t("qualityHeading")}</h5>
+                <p className="tracking-widest leading-6">{t("qualityText")}</p>
             </div>
 
             <div className="flex flex-col justify-start items-start gap-2">
-                <h5 className="font-medium font-secondary">Speed & reliability:</h5>
-                <p className="tracking-widest leading-6">We offer you fast processing times and reliable execution - so that your watch is back with you quickly.</p>
+                <h5 className="font-medium font-secondary">{t("speedHeading")}</h5>
+                <p className="tracking-widest leading-6">{t("speedText")}</p>
             </div>
         </div>
     );
