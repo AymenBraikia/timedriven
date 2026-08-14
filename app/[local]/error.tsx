e16141cb224ec350f43fb9f44a242ec28ecdfcb4
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-08-14 14:45:25 +0100

    translated last added pages

--- app/[local]/error.tsx
+++ app/[local]/error.tsx
@@ -1,30 +1,28 @@
-"use client"
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
-import "@/app/globals.css";
+import { useTranslations } from "next-intl";
 
-export default function NotFound() {
+export default function Error({ reset }: { reset: () => void }) {
+    const t = useTranslations("error")
     return (
         <div className="flex-center py-10">
             <div className="relative aspect-video w-dvw">
                 <div className="absolute top-1/7 right-1/8 flex justify-center items-start flex-col fade-in p-8 max-w-150 liquid-glass">
-                    <span className="text-xs uppercase tracking-widest font-semibold text-secondary">404 — Page Not Found</span>
-
-                    <h1 className="mt-2 text-2xl md:text-3xl font-secondary font-semibold">Time Has Run Out for This Page</h1>
-
-                    <p className="mt-3 text-secondary text-sm md:text-base leading-relaxed">The page or timepiece you are searching for has been moved, renamed, or no longer exists.</p>
-
-                    <div className="flex gap-4 mt-6">
-                        <Link href="/collection" className="button">
-                            Explore Our Collection
-                        </Link>
-                        <Link href="/" className="button2">
-                            Return Home
+                    <h1>{t("heading")}</h1>
+                    <h5 className="font-secondary">{t("subHeading")}</h5>
+                    <p className="mt-2 text-secondary">{t("subText")}</p>
+                    <div className="flex gap-4">
+                        <button onClick={() => reset()} className="mt-4 px-4 py-2 button capitalize">
+                            {t("tryAgain")}
+                        </button>
+                        <Link href="/" className="mt-4 px-4 py-2 button2 capitalize">
+                            {t("return")}
                         </Link>
                     </div>
                 </div>
-
-                <Image fill priority alt="Luxury timepiece background" src="/notFound.jpg" sizes="100dvw" className="object-cover object-center -z-10 brightness-80" />
+                <Image fill alt="Error" src={"/notFound.jpg"} sizes="100dvw" className="object-cover object-center -z-10 brightness-80" />
             </div>
         </div>
     );
