02e08848c3ae2d077bc9e724042ae012db94d7ad
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-08-14 14:53:00 +0100

    translated last added pages

--- app/[local]/(site)/[...rest]/page.tsx
+++ app/[local]/(site)/[...rest]/page.tsx
@@ -1,8 +1,5 @@
-// app/[local]/[...rest]/page.tsx
 import { notFound } from 'next/navigation'
 
 export default function CatchAllRoute() {
-  // This explicitly catches any unmatched URL under /[local]/...
-  // and forces Next.js to render your app/[local]/not-found.tsx
   notFound()
 }
