f13480557ae900b8c1649fe25eeacb524bb6a5fa
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-08-26 15:44:46 +0100

    save

--- app/[local]/components/booking.tsx
+++ app/[local]/components/booking.tsx
@@ -9,7 +9,7 @@ export default function Booking() {
     const t = useTranslations("home.bookingBanner");
     return (
         <FadeInObserver>
-            <div className={`w-full min-h-100 h-125 my-12 sm:my-20 relative flex-center flex-col text-center text-white select-none font-secondary gap-4 px-4`}>
+            <div className={`w-full min-h-100 h-125 mt-12 sm:mt-20 relative flex-center flex-col text-center text-white select-none font-secondary gap-4 px-4`}>
                 <Image src={booking_img} quality={60} alt="booking.webp" sizes="100vw" className="object-cover object-center select-none brightness-30" fill></Image>
                 <p className="z-10 sm:text-5xl text-4xl tracking-wider">{t("heading")}</p>
                 <p className="z-10 sm:text-2xl text-xl tracking-wide leading-8">{t("subtext")}</p>
