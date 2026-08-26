4a55a75ac1c67d32c469a64611b18ee92ad7475a
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-08-26 15:25:41 +0100

    hardcodded centered inspired.tsx

--- app/[local]/components/inspired.tsx
+++ app/[local]/components/inspired.tsx
@@ -9,11 +9,11 @@ export default function Inspired(){
     return (
         <FadeInObserver>
             <div className="px-4 py-12 flex-center flex-col md:flex-row min-h-100 gap-5 md:h-50 md:gap-20 overflow-hidden text-center relative">
-                <p className="text-4xl sm:text-6xl tracking-wider">{t("getInspiredHeading")}</p>
+                <p className="text-4xl sm:text-6xl tracking-wider w-1/2">{t("getInspiredHeading")}</p>
                 <div className="md:h-full md:w-0.5 w-full h-1 bg-foreground"></div>
-                <div className="text-xl flex flex-col gap-8 h-1/2 font-secondary">
+                <div className="text-xl flex flex-col gap-8 h-1/2 font-secondary w-1/2">
                     <p className="max-w-100 wrap-break-word tracking-wide leading-8 text-shine">{t("getInspiredSubtext")}</p>
-                    <Link aria-label="Begin your journey" className="underline text-shine" href={"/shop"}>
+                    <Link aria-label="Begin your journey" className="underline text-shine w-full max-w-100" href={"/shop"}>
                         {t_btn("begin")}
                     </Link>
                 </div>
