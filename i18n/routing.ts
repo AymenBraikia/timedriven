191b519dc177db98956a18e31ed4f4ab6bacf72c
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-09-03 21:44:10 +0100

    back again

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,7 +2,7 @@ import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-    locales: ["en", "de", "ar", "fr", "tr", "it"],
+    locales: ["en", "de", "es", "ar", "fr", "tr", "it"],
     defaultLocale: "en",
 });
 
