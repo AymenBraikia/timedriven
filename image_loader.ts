b48d79efd5360258b3b9f9683ccffdd1548545ed
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-09-04 20:26:52 +0100

    sick n tired

--- image_loader.ts
+++ image_loader.ts
@@ -1,6 +1,11 @@
+// image-loader.ts
 export default function imageLoader({ src, width, quality }: { src: string; width: number; quality?: number }) {
-    // If using relative paths, prepend your production domain
-    const fullSrc = src.startsWith("http") ? src : `${process.env.NODE_ENV == "production" ? "https://arvell.vercel.app" : "http://localhost:3000"}${src}`;
+    if (process.env.NODE_ENV === "development") {
+        return src;
+    }
+
+    const productionDomain = "https://arvell.vercel.app";
+    const fullSrc = src.startsWith("http") ? src : `${productionDomain}${src}`;
 
     return `https://wsrv.nl/?url=${encodeURIComponent(fullSrc)}&w=${width}&q=${quality || 75}&output=webp`;
 }
