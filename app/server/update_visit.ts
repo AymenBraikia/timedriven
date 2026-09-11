cc573cbc59d6ec6683be29660f82505368d50d71
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-09-11 16:36:07 +0100

    added pop up monitoring

--- app/server/update_visit.ts
+++ app/server/update_visit.ts
@@ -1,9 +1,10 @@
+"use server"
 import { cookies } from "next/headers";
 import { visitors_collection } from "../db/collections";
 
 export async function update_visit() {
     if (process.env.NODE_ENV != "production") return;
-    
+
     const collection = await visitors_collection();
 
     const cookieStore = await cookies();
