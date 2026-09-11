00074872a0029595bdbf120dbc866ce02dbf1748
Aymen-Braikia <aymenbraikia1@gmail.com>
2026-09-11 16:34:21 +0100

    added pop up monitoring

--- app/server/update_visit.ts
+++ app/server/update_visit.ts
@@ -2,14 +2,14 @@ import { cookies } from "next/headers";
 import { visitors_collection } from "../db/collections";
 
 export async function update_visit() {
-    if (process.env.NODE_ENV == "production") {
-        const collection = await visitors_collection();
-        
-        const cookieStore = await cookies();
+    if (process.env.NODE_ENV != "production") return;
+    
+    const collection = await visitors_collection();
 
-        const referer = cookieStore.get("ref");
-        if (!referer) return;
+    const cookieStore = await cookies();
 
-        collection.updateOne({ referer }, { $set: { saw_pop_up: true } });
-    }
+    const referer = cookieStore.get("ref");
+    if (!referer) return;
+
+    collection.updateOne({ referer }, { $set: { saw_pop_up: true } });
 }
